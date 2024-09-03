eaed365991c4b648fd6dabef1132aa15b5255768
Victor-Maverick <victormsonter@gmail.com>
2024-09-03 10:10:57 +0100

    first commit

---
+++ src/main/java/africa/semicolon/clouddrill/CloudDrillApplication.java
@@ -0,0 +1,13 @@
+package africa.semicolon.clouddrill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDrillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudDrillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/africa/semicolon/clouddrill/CloudDrillApplicationTests.java
@@ -0,0 +1,13 @@
+package africa.semicolon.clouddrill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudDrillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
