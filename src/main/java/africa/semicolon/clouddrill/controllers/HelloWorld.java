4f9afe0b9c511add3a5b781610e722aaef5db498
Victor-Maverick <victormsonter@gmail.com>
2024-09-03 14:53:59 +0100

    commit

--- src/main/java/africa/semicolon/clouddrill/controllers/HelloWorld.java
+++ src/main/java/africa/semicolon/clouddrill/controllers/HelloWorld.java
@@ -1,5 +1,6 @@
 package africa.semicolon.clouddrill.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/hello")
 public class HelloWorld {
-    @PostMapping
+    @GetMapping
     public String sayHello() {
         return "Hello World!";
     }
