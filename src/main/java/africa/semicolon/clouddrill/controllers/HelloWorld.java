e89737206ed7f7bd6c5c1e3b3dcb02ebb59a5d48
Victor-Maverick <victormsonter@gmail.com>
2024-09-03 12:12:07 +0100

    sayHello method added

---
+++ src/main/java/africa/semicolon/clouddrill/controllers/HelloWorld.java
@@ -0,0 +1,14 @@
+package africa.semicolon.clouddrill.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/hello")
+public class HelloWorld {
+    @PostMapping
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
